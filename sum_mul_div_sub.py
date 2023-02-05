74e74d494d324d376c0534a266f94857ea959d72
NeelamSE <122192725+NeelamSE@users.noreply.github.com>
2023-02-05 19:36:40 +0530

    Add files via upload

---
+++ sum_mul_div_sub.py
@@ -0,0 +1,15 @@
+a=int(input('enter the number'))
+b=int(input('enter the number'))
+
+c=a+b
+print('sum of{} and {} are={}'.format(a,b,c))
+
+
+d=a-b
+print('subtration of {} and {} are={}'.format(a,b,d))
+
+e=a*b
+print('multiplicaton of {} and {} are ={}'.format(a,b,e))
+
+f=a/b
+print('division of {} and {} are ={}'.format(a,b,f))
---
+++ table.py
@@ -0,0 +1,5 @@
+a=int(input('enter number any number'))
+for x in range(a,a*11,a):
+    print(x)
+
+
