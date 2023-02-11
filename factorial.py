d3a4133688922f6b8339dd22cb73c45a31ae1f9a
NeelamSE <122192725+NeelamSE@users.noreply.github.com>
2023-02-11 20:22:57 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,5 @@
+fact =1
+a=int(input("enter the number"))
+for x in range(a,0,-1):
+    fact=fact*x
+print(fact)
---
+++ fibonacci_series.py
@@ -0,0 +1,11 @@
+n=int(input('enter the number'))
+a=0
+print(a)
+b=1
+print(b)
+for x in range(2,n):
+    feb=a+b
+    print(feb)
+    a=b
+    b=feb
+
